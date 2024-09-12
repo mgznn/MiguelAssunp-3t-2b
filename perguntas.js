f7c66a3ca3d9f3552974067df98e3bf3d0a7c608
Miguel <141921930+mgznn@users.noreply.github.com>
2024-09-12 16:27:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é o melhor jogador do mundo?',
+    'Messi'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital da Brasil é brasilia'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual a maior liga de futebol?',
+    'Premier League'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz futebol em Inglês?',
+    'Futebol em ingles é Soccer'
+)
